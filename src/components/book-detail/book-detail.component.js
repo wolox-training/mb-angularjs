0da1c7e6197255d81911f70538527e99994cd759
MicaelaBalza <micaela.balza@wolox.com.ar>
2021-03-11 17:28:28 -0300

    pr requested changes and added search icon

--- src/components/book-detail/book-detail.component.js
+++ src/components/book-detail/book-detail.component.js
@@ -1,3 +1,6 @@
+import bookCover from './../../assets/img/book-cover.png';
+import badge from './../../assets/img/badge.png';
+
 const angular = require('angular');
 
 angular.module('app-bootstrap')
@@ -5,8 +8,8 @@ angular.module('app-bootstrap')
     template: require('./book-detail.html'),
     controller: ['$stateParams', 'booksService', function ($stateParams, booksService) {
       let bookId = parseInt($stateParams.bookId);
-      this.bookCover = require('./../../assets/img/book-cover.png');
-      this.badge = require('./../../assets/img/badge.png');
+      this.bookCover = bookCover;
+      this.badge = badge;
       this.book = booksService.getBook(bookId);
     }]
   });
--- src/components/books/books.component.js
+++ src/components/books/books.component.js
@@ -1,23 +1,17 @@
+import bookCover from './../../assets/img/book-cover.png';
+
 const angular = require('angular');
 
 angular.module('app-bootstrap')
   .component('books', {
     template: require('./books.html'),
     controller: ['booksService', function (booksService) {
-      this.bookCover = require('./../../assets/img/book-cover.png');
+      this.bookCover = bookCover;
       this.books = booksService.getBooksList();
     }]
   })
   .filter('searchBook', function() {
-    return function (items, searchText) {
-      let filtered = [];
-      let letterMatch = new RegExp(searchText, 'i');
-      for (var i = 0; i < items.length; i++) {
-        var item = items[i];
-        if (letterMatch.test(item.title) || letterMatch.test(item.author)) {
-          filtered.push(item);
-        }
-      }
-      return filtered;
+    return function(items, searchText) {
+      return items.filter(item => searchText ? item.title.toLowerCase().includes(searchText.toLowerCase()) || item.author.toLowerCase().includes(searchText.toLowerCase()) : true);
     };
   });
